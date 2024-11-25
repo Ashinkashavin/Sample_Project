8917768b8f027da09aea73d6ed969c1858a4a6f7
Ashinkashavin <shavinmeepagala@gmail.com>
2024-11-25 23:32:25 +0530

    2nd commit

--- sample_project/app/Http/Controllers/VehicleController.php
+++ sample_project/app/Http/Controllers/VehicleController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Vehicle;
+use App\Models\User;
+
 
 class VehicleController extends Controller
 {
-    //
+        // View all vehicles and display the total number of users and vehicles
+        public function index()
+        {
+            // Get all vehicles
+            $vehicles = Vehicle::all();
+    
+            // Count total number of users and vehicles
+            $totalUsers = User::count();
+            $totalVehicles = Vehicle::count();
+    
+            // Return to the view with data
+            return view('vehicles.index', compact('vehicles', 'totalUsers', 'totalVehicles'));
+        }
+    
+        // Delete a vehicle by ID
+        public function destroy($id)
+        {
+            // Find the vehicle and delete it
+            $vehicle = Vehicle::find($id);
+            if ($vehicle) {
+                $vehicle->delete();
+                return redirect()->route('vehicles.index')->with('success', 'Vehicle deleted successfully.');
+            }
+            return redirect()->route('vehicles.index')->with('error', 'Vehicle not found.');
+        }
 }
